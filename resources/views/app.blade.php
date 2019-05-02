2d7b75ce338052f9bbb56b7433285002d9c86f9d
Egor <egor@saiware.com>
2019-05-03 01:27:02 +0300

    create Admin part

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,16 +6,23 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{env('APP_NAME')}}</title>
-
-        <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
         <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
+        <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
         <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
+        <link href="{{asset('/demo/demo.css')}}" rel="stylesheet" type="text/css">
     </head>
     <body>
-       <div id="app">
-          <app></app>
-       </div>
-       <script src="{{asset('js/app.js')}}"></script>
+    <section id="app">
+        @yield('content')
+    </section>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.bundle.js"></script>
+    <script src="{{asset('js/app.js')}}"></script>
+    <script>
+        $(document).ready(function() {
+            demo.initLandingPageChart();
+        });
+    </script>
     </body>
 </html>
